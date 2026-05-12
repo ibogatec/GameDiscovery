d4533d702e7fdf18e96cf893dac495b745d1fe6e
Ivan Bogatec <ivan.bogatec@gmail.com>
2026-05-12 19:14:05 +0200

    setup Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
     return (
         <div>
             <h1>Game Discovery</h1>
+            <Button colorPalette='blue'>Hello Chakra</Button>
         </div>
     );
 }
