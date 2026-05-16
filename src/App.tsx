e3754151773e634a2602cfafb4536e14c3dc9627
Ivan Bogatec <ivan.bogatec@gmail.com>
2026-05-16 13:30:40 +0200

    responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
     return (
-        <div>
-            <h1>Game Discovery</h1>
-            <Button colorPalette='blue'>Hello Chakra</Button>
-        </div>
+        <Grid templateAreas={
+            {
+                base: "'nav' 'main'",
+                lg: "'nav nav' 'aside main'",
+            }}>
+            <GridItem area="nav" bg="coral">
+                Nav
+            </GridItem>
+
+            <GridItem area="aside" bg="gold" hideBelow="lg">
+                Aside
+            </GridItem>
+
+            <GridItem area="main" bg="dodgerblue">
+                Main
+            </GridItem>
+        </Grid>
     );
 }
 
