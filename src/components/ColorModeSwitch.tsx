5e9a09c6d1ef0f8440d9777726487feb4d4df0e5
Ivan Bogatec <ivan.bogatec@gmail.com>
2026-05-27 20:05:33 +0200

    implement color mode switch

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,34 @@
+import { Switch } from "@chakra-ui/react";
+import { useColorMode } from "./ui/color-mode.tsx";
+
+function ColorModeSwitch() {
+    const { colorMode, setColorMode } = useColorMode();
+    const isDark = colorMode === "dark";
+    return (
+        <Switch.Root
+            checked={isDark}
+            onCheckedChange={({ checked }) =>
+                setColorMode(checked ? "dark" : "light")
+            }
+            size="sm">
+            <Switch.HiddenInput />
+
+            <Switch.Control
+                bg="gray.400"
+                _checked={{ bg: "green.600" }}>
+                <Switch.Thumb
+                    bg="white"
+                    _checked={{ bg: "white" }}
+                    transitionProperty="translate"
+                    transitionDuration="0.3s"
+                    transitionTimingFunction="ease-in-out"/>
+            </Switch.Control>
+
+            <Switch.Label>
+                {isDark ? "Dark mode" : "Light mode"}
+            </Switch.Label>
+        </Switch.Root>
+    );
+}
+
+export default ColorModeSwitch;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch.tsx";
 
 function NavBar() {
     return (
-        <HStack>
+        <HStack justifyContent="space-between" px="4">
             <Image src={logo} alt="Logo" boxSize="64px" />
             <Text>NavBar</Text>
+            <ColorModeSwitch />
         </HStack>
     );
 }
